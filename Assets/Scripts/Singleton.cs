64b333d7ef97684367b25539ae086c58c2ec2ca0
3047322836 <3047322836@qq.com>
2025-06-08 01:05:35 +0800

    deleted the original

--- Assets/Scripts/Singleton.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Singleton<T> : MonoBehaviour where T:MonoBehaviour{
-    public static T inst;
-    protected virtual void Awake(){
-        inst=this as T;
-    }
-}
