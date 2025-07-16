3f14b479787ddf97cb37634adab890b9e0a0a769
Sapiens-wx <wx2194@nyu.edu>
2025-07-16 09:33:03 +0800

    add background and some juice to HideNSeek game

--- Assets/Scripts/HideNSeek/ScoreRecorder.cs
+++ Assets/Scripts/HideNSeek/ScoreRecorder.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
 using TMPro;
 using ResearchUtilities;
+using System.Collections;
 
 namespace HideNSeek{
 public class ScoreRecorder : Singleton<ScoreRecorder>{
     [Header("Score")]
     public TMP_Text text_score;
+    public TMP_Text tips;
+    public float tipsDuration, tipsSpd;
 
     bool inRound;
     int round;
     float score;
     float formulaScore, formulaScore_sum, maxDist, v_bar, v_bar_samples;
+    Coroutine tipsCoro;
     void Start(){
         text_score.text="Score/Rounds: 0/0";
+        tips.gameObject.SetActive(false);
     }
     void OnDisable(){
-        EventCollector.Instance.CollectAllEventLogsToFile();
+        //EventCollector.Instance.CollectAllEventLogsToFile();
     }
     void FixedUpdate(){
         if(inRound){
@@ -34,13 +39,37 @@ public class ScoreRecorder : Singleton<ScoreRecorder>{
         inRound=false;
         ++round;
         //update score
-        if(success) score+=1;
+        if(success){
+            score+=1;
+            if(tipsCoro!=null) StopCoroutine(tipsCoro);
+            tipsCoro=StartCoroutine(TipAnim(new Vector3(GameManager.inst.onTheLeft?GameManager.inst.leftKid.transform.position.x:GameManager.inst.rightKid.transform.position.x,tips.transform.position.y,0)));
+        }
         v_bar/=v_bar_samples*PlayerCtrl.inst.MoveSpd;
         formulaScore_sum+=maxDist*v_bar;
         formulaScore=formulaScore_sum/round;
         text_score.text="Score/Rounds: "+score.ToString("0")+"/"+round.ToString();
-        text_score.text=$"Score/Rounds: {score.ToString("0")}/{round}\nAverage Formula Score: {formulaScore}";
-        EventCollector.Instance.RecordEvent($"Round Results", $"Round {round}: {text_score.text}");
+        text_score.text=$"Score/Rounds: {score.ToString("0")}/{round}\nControllability Score: {formulaScore}";
+        //EventCollector.Instance.RecordEvent($"Round Results", $"Round {round}: {text_score.text}");
+    }
+    IEnumerator TipAnim(Vector3 startPos){
+        WaitForFixedUpdate wait=new WaitForFixedUpdate();
+        float spd=tipsSpd*Time.fixedDeltaTime;
+        float toTime=Time.time+tipsDuration;
+        Color originalColor=tips.color, curColor=originalColor;
+        Vector3 displacement=new Vector3(0,spd,0);
+        float normalizedDt=Time.fixedDeltaTime/tipsDuration;
+        tips.transform.position=startPos;
+        tips.gameObject.SetActive(true);
+        while(Time.time<toTime){
+            tips.transform.position+=displacement;
+            curColor.a-=normalizedDt;
+            tips.color=curColor;
+            yield return wait;
+        }
+        tips.gameObject.SetActive(false);
+        tips.transform.position=startPos;
+        tips.color=originalColor;
+        tipsCoro=null;
     }
 }
 }
